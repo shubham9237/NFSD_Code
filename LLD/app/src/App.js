5e818d497eaf80c317d61d76ff39c8de0ca8aaef
shubham9237 <l.shubham9237@gmail.com>
2025-01-29 09:17:19 +0530

    [SL] Added the NFSD Tutorials and code

--- LLD/app/src/App.js
+++ LLD/app/src/App.js
@@ -1,52 +1,58 @@
-import About from "./components/About";
-import Body from "./components/Body";
+import About from "./Study_CMP/About";
+import Body from "./Study_CMP/Body";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Team from "./components/Team";
-import Login from "./components/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
+import Team from "./Study_CMP/Team";
+import Login from "./Study_CMP/Login";
+import ProtectedRoute from "./Study_CMP/ProtectedRoute";
 import { useState } from "react";
-import Accordion from "./components/Accordion";
-import Comments from "./components/comments/Comments";
-import ImageSlider from "./components/image-slider/ImageSlider";
+import Comments from "./Study_CMP/comments/Comments";
+import ImageSlider from "./Study_CMP/Image-slider/ImageSlider";
+import Pagination from "./Study_CMP/Pagination/Pagination";
+import LiveChat from "./Study_CMP/LiveChat/LiveChat";
+import Accordion from "./Study_CMP/Accordion";
+import SearchUI from "./Study_CMP/searchUI/SearchUI";
 
 function App() {
   const [lang, setLang] = useState("en");
-  return (
-    <div>
-      <header className="text-2xl font-bold py-5 bg-black text-white text-center flex">
-        Hello World
-        <nav className="px-20 m-2 w-[800px] flex justify-between text-lg">
-          <a href="/">Home </a>
-          <a href="/about">About </a>
-          <a href="/accordion">Accordion </a>
-          <a href="/comments">Nested Comments </a>
-          <a href="/image-slider">Image Slider </a>
-          <a href="/team">Team </a>
-          <a href="/login">Login </a>
-        </nav>
-        <select value={lang} onChange={(e) => setLang(e.target.value)}>
+  return <div>
+    <header className="text-2xl font-bold py-5 bg-black text-white text-center flex justify-between">
+      <h1>Hello World</h1>
+      <nav className="p-2 m-2 w-[80%] text-md flex justify-between">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/accordian">Accordian</a>
+        <a href="/image-slider">ImageSlider</a>
+        <a href="/team">Team</a>
+        <a href="/comments">Nested Comments</a>
+        <a href="/pagination">Pagination</a>
+        <a href="/live-chat">Live Chat</a>
+        <a href="/search-ui">SearchUI</a>
+        <a href="/login">Login</a>
+        <select value={lang} onChange={(e)=>setLang(e.target.value)} className="font-black">
           <option value="en">English</option>
           <option value="hi">Hindi</option>
           <option value="sp">Spanish</option>
           <option value="ru">Russian</option>
         </select>
-      </header>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}></Route>
-          <Route element={<ProtectedRoute />}>
-            <Route path="/team" element={<Team />}></Route>
-          </Route>
-          <Route path="/about" element={<About lang={lang} />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/accordion" element={<Accordion />}></Route>
-          <Route path="/comments" element={<Comments />}></Route>
-          <Route path="/image-slider" element={<ImageSlider />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+      </nav>
+    </header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Body />}></Route>
+        <Route path="/about" element={<About lang={lang} />}></Route>
+        <Route path="/accordian" element={<Accordion />}></Route>
+        <Route path="/image-slider" element={<ImageSlider />}></Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/team" element={<Team />}></Route>
+        </Route>
+        <Route path="/comments" element={<Comments />}></Route>
+        <Route path="/pagination" element={<Pagination />}></Route>
+        <Route path="/live-chat" element={<LiveChat />}></Route>
+        <Route path="/search-ui" element={<SearchUI />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
